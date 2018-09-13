ff1ba415097dd9f160c94ac54bbec459563ed0ab
snowbird4 <rokas.mikalauskas95@gmail.com>
2018-09-13 16:59:11 +0100

    added list component and router

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
-    <add-profile/>
+    <!--<add-profile/>-->
+    <list-profiles/>
   </div>
 </template>
 
 <script>
 import addProfile from './components/addProfile.vue'
+import listProfiles from './components/listProfiles.vue'
 export default {
   components: {
-    'add-profile':addProfile
+    'add-profile':addProfile,
+    'list-profiles': listProfiles,
   },
   data () {
     return {
---
+++ src/components/listProfiles.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="listProfiles">
+  <h1> Profiles </h1>
+  <div v-for="profile in profiles" class="profile">
+    <h4> {{profile.name}} </h4>
+    <p> {{profile.surname}} </p>
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+
+  },
+  data () {
+    return {
+      profiles: []
+    }
+  },
+  methods: {
+
+  },
+  created() {
+    this.$http.get('firebase link').then(function(data){
+      this.profiles = data.body.slice(0,5)
+    })
+    }
+  }
+</script>
+
+<style scoped>
+
+
+
+</style>
